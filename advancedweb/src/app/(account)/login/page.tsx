6fb9793114cf96a5033983da110a6bdf7b78f149
kong33 <kge021114@naver.com>
2025-04-11 17:12:56 +0200

    Feat: login page

--- advancedweb/src/app/(account)/login/page.tsx
+++ advancedweb/src/app/(account)/login/page.tsx
@@ -1,13 +1,18 @@
+"use client";
+import Button from "@/component/button";
 import Input from "@/component/input";
 
 export default function Login() {
+  const handleButtonClick = () => {
+    //send request.
+  };
   return (
-    <div>
+    <div className="inputWrapper">
       <section>
-        <Input size="s" label="hello" placeholder="hello" type="text" />
-        <Input size="m" label="hello" placeholder="hello" type="text" />
-        <Input size="l" label="hello" placeholder="hello" type="text" />
+        <Input size="s" label="id" placeholder="Please enter the id" type="text" />
+        <Input size="s" label="password" placeholder="Please enter the password" type="text" />
       </section>
+      <Button name="login" size="m" handleButtonClick={handleButtonClick} />
     </div>
   );
 }
