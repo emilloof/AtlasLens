3033088c4f18f796ebbc778997cd1186defb4a3f
emilo851 <emilo851@student.liu.se>
2025-04-15 13:41:56 +0200

    Fix: merge + fixed file structure

--- advancedweb/src/app/map/page.tsx
+++ advancedweb/src/app/map/page.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic'
 
-const MapComponent = dynamic(() => import('@/components/LeafletMap'), { ssr: false })
+const MapComponent = dynamic(() => import('@/component/LeafletMap'), { ssr: false })
 
 export default function Page() {
   return <MapComponent />
