653a097964088619a19ad860a4391f53f3c6ed35
emilo851 <emilo851@student.liu.se>
2025-04-19 00:15:24 +0200

    Fix: img to Image + custom popup css

--- advancedweb/src/component/albumPreview.tsx
+++ advancedweb/src/component/albumPreview.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import styles from './albumPreview.module.css';
+import Image from 'next/image';
 
-interface PhotoStackProps {
+interface AlbumPreviewProps {
   images: string[];
   width?: number;
   height?: number;
 }
 
-const AlbumPreview: React.FC<PhotoStackProps> = ({ images, width = 150, height = 100 }) => {
+const AlbumPreview: React.FC<AlbumPreviewProps> = ({ images, width = 150, height = 100 }) => {
   images = images.slice(0, 5);
   return (
     <div className={styles.stack} style={{ width, height }} onClick={() => {/*Handle album click*/}}>
       {images.map((src, index) => (
-        <img
+        <Image
           key={index}
           src={src}
           alt={`Photo ${index + 1}`}
           className={styles.photo}
+          width={width}
+          height={height}
           style={{
             zIndex: images.length - index,
             top: index * 5,
