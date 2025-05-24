c4ee9ebaae33b3feb2d4dc6f5fc3c972b22b5e76
kong33 <kge021114@naver.com>
2025-05-24 15:32:26 +0200

    Fix: secreat key logic change to throwing error

--- advancedweb/src/app/api/login/route.ts
+++ advancedweb/src/app/api/login/route.ts
@@ -18,22 +18,23 @@ export async function POST(req: Request) {
     if (!user || !(await bcrypt.compare(password, user.password))) {
       return NextResponse.json({ message: "Invalid credentials" }, { status: 401 });
     }
+    if (SECRET_KEY) {
+      const token = jwt.sign({ user_id: user.user_id, email: user.email }, SECRET_KEY, { expiresIn: "1h" });
 
-    const token = jwt.sign({ email }, SECRET_KEY, { expiresIn: "1h" });
+      const response = NextResponse.json({ message: "Login successful", status: 200 });
 
-    const response = NextResponse.json({ message: "Login successful", status: 200 });
-    console.log("response", response);
-    response.cookies.set("access_token", token, {
-      httpOnly: true,
-      secure: false,
-      // secure: process.env.NODE_ENV === "production", // Ensure secure cookie in production
-      // sameSite: "strict",
-      sameSite: "none",
-      path: "/",
-      maxAge: 60 * 60,
-    });
-    console.log("token", token);
-    return response;
+      response.cookies.set("access_token", token, {
+        httpOnly: true,
+        secure: false,
+        // secure: process.env.NODE_ENV === "production", // Ensure secure cookie in production
+        // sameSite: "strict",
+        sameSite: "none",
+        path: "/",
+        maxAge: 60 * 60,
+      });
+
+      return response;
+    }
   } catch (error) {
     return NextResponse.json({ message: `Error: ${error}` }, { status: 500 });
   }
