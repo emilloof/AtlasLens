10e5fecb7b6168f047c2500476d4f38b911f2934
emilo851 <emilo851@student.liu.se>
2026-05-30 10:45:53 +0200

    Fix: landing page cleanup

--- advancedweb/src/middleware.ts
+++ advancedweb/src/middleware.ts
@@ -16,6 +16,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|login|signup|api|map/public|.*\\..*).*)"
+    "/((?!_next/static|_next/image|favicon.ico|login|signup|api|map/public|$|.*\\..*).*)"
   ],
 };
